5df4e35e1a596e57f06c74c24c1856ceaad9265c
Ibrahim Hawari <ibrahimhawari@Ibrahims-MBP.attlocal.net>
2017-10-11 23:34:59 -0400

    improved structure of baseRoute

--- routes/baseRoute.js
+++ routes/baseRoute.js
@@ -3,53 +3,52 @@ let express = require('express');
  * Extend this abstract class with a class extending baseModel
  */
 class baseRoute {
-  constructor(dbModel, callbacks = {
-    create: (req, res) => {
+  constructor(dbModel) {
+    this.router = express.Router();
+    this.create = (req, res) => {
       let m = new dbModel.model(req.body)
       m.save((err, data) => {
         if (err)
           res.status(400).send(err);
         res.status(201).send(data);
       })
-    },
-    read: (req, res) => {
+    }
+    this.read = (req, res) => {
       dbModel.model.find({}, (err, data) => {
         if (err)
           res.status(400).send(err);
         res.status(200).send(data);
       })
-    },
-    readOne: (req, res) => {
+    }
+    this.readOne = (req, res) => {
       dbModel.model.findById(req.params.id, (err, data) => {
         if (err)
           res.status(400).send(err);
         res.status(200).send(data);
       })
-    },
-    update: (req, res) => {
+    }
+    this.update = (req, res) => {
       dbModel.model.where({ _id: req.params.id }).update(req.body, (err, data) => {
         if (err)
           res.status(400).send(err);
         res.status(201).send(data);
       })
-    },
-    delete: (req, res) => {
+    }
+    this.delete = (req, res) => {
       dbModel.model.remove({ _id: req.params.id }, (err, data) => {
         if (err)
           res.stauts(400).send(err);
         res.status(200).send(data);
       })
     }
-  }) {
-    this.router = express.Router();
-    this.init(callbacks);
+    this.init();
   }
-  init(callbacks) {
-    this.router.post('/', callbacks.create);    
-    this.router.get('/', callbacks.read);
-    this.router.get('/:id', callbacks.readOne);
-    this.router.put('/:id', callbacks.update);
-    this.router.delete('/:id', callbacks.delete);
+  init() {
+    this.router.post('/', this.create);
+    this.router.get('/', this.read);
+    this.router.get('/:id', this.readOne);
+    this.router.put('/:id', this.update);
+    this.router.delete('/:id', this.delete);
   }
 }
 
