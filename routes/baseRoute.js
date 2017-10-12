1a7cd36996e5ce08cc09a89256aebd9d57e95ba8
Ibrahim Hawari <ibrahimhawari@Ibrahims-MBP.attlocal.net>
2017-10-11 22:47:33 -0400

    added callback definition to default params

--- routes/baseRoute.js
+++ routes/baseRoute.js
@@ -3,46 +3,46 @@ let express = require('express');
  * Extend this abstract class with a class extending baseModel
  */
 class baseRoute {
-  constructor(dbModel) {
+  constructor(dbModel, callbacks = {
+    create: (req, res) => {
+      let m = new dbModel.model(req.body)
+      m.save((err, data) => {
+        if (err)
+          res.status(400).send(err);
+        res.status(201).send(data);
+      })
+    },
+    read: (req, res) => {
+      dbModel.model.find({}, (err, data) => {
+        if (err)
+          res.status(400).send(err);
+        res.status(200).send(data);
+      })
+    },
+    readOne: (req, res) => {
+      dbModel.model.findById(req.params.id, (err, data) => {
+        if (err)
+          res.status(400).send(err);
+        res.status(200).send(data);
+      })
+    },
+    update: (req, res) => {
+      dbModel.model.where({ _id: req.params.id }).update(req.body, (err, data) => {
+        if (err)
+          res.status(400).send(err);
+        res.status(201).send(data);
+      })
+    },
+    delete: (req, res) => {
+      dbModel.model.remove({ _id: req.params.id }, (err, data) => {
+        if (err)
+          res.stauts(400).send(err);
+        res.status(200).send(data);
+      })
+    }
+  }) {
     this.router = express.Router();
-    this.init({
-      create: (req, res) => {
-        let m = new dbModel.model(req.body)
-        m.save((err, data) => {
-          if (err)
-            res.status(400).send(err);
-          res.status(201).send(data);
-        })
-      },
-      read: (req, res) => {
-        dbModel.model.find({}, (err, data) => {
-          if (err)
-            res.status(400).send(err);
-          res.status(200).send(data);
-        })
-      },
-      readOne: (req, res) => {
-        dbModel.model.findById(req.params.id, (err, data) => {
-          if (err)
-            res.status(400).send(err);
-          res.status(200).send(data);
-        })
-      },
-      update: (req, res) => {
-        dbModel.model.where({ _id: req.params.id }).update(req.body, (err, data) => {
-          if (err)
-            res.status(400).send(err);
-          res.status(201).send(data);
-        })
-      },
-      delete: (req, res) => {
-        dbModel.model.remove({ _id: req.params.id }, (err, data) => {
-          if (err)
-            res.stauts(400).send(err);
-          res.status(200).send(data);
-        })
-      }
-    });
+    this.init(callbacks);
   }
   init(callbacks) {
     this.router.post('/', callbacks.create);    
