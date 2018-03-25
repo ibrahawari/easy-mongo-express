4663900ad4f4c93d83443ea803d684abc5ceae0d
Ibrahim Hawari <ibrahimhawari@Ibrahims-MBP.attlocal.net>
2018-03-25 14:18:38 -0400

    skipping the html step and going straight for storing text

--- models/webDataModel.js
+++ models/webDataModel.js
@@ -1,13 +1,12 @@
 let baseModel = require('./baseModel');
 /**
- * MongoDB model for HTML data retrieved from a url
+ * MongoDB model for text data retrieved from a url
  */
 class webDataModel extends baseModel {
   constructor() {
     super('webData', {
       url: { type: String, required: true },
-      data: { type: String, required: true },
-      text_content: { type: String, required: true },
+      data: {type: String, required: true}
     });
   }
 }
--- routes/webDataRoute.js
+++ routes/webDataRoute.js
@@ -2,8 +2,9 @@ let baseRoute = require('./baseRoute');
 let webDataModel = require('../models/webDataModel');
 let cheerio = require('cheerio');
 let request = require('request');
+let _ = require('lodash');
 /**
- * Express route for storing data scraped from websites in mongodb
+ * Express route for storing text data scraped from websites in mongodb
  */
 class webDataRoute extends baseRoute {
     constructor() {
@@ -17,10 +18,8 @@ class webDataRoute extends baseRoute {
             }, function (err, response, body) {
                 if (err)
                     res.status(400).send(err);
-                
                 let $ = cheerio.load(body);
-                req.body.data = body;
-                req.body.text_content = $.text();
+                req.body.data = _.lowerCase($.text());
                 let m = new dbModel.model(req.body);
                 m.save((err, data) => {
                     if (err)
